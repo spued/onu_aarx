9799b78aeb49d907422e0419f580a8d3d0314ff5
Sunya J <sun_tct@hotmail.com>
2023-02-24 12:05:40 +0700

    try to delete dups but no good

--- controller/import_csv.js
+++ controller/import_csv.js
@@ -22,47 +22,54 @@ const import_csv = {
   	.pipe(csv())
   	.on('data', (data) => results.push(data))
   	.on('end', () => {
-                let sql = "INSERT INTO aarx_master (uuid,filename,qty,original_filename) VALUES ( '"
-                                         + uuid
-                                         + "', '" + req.file.filename
-                                         + "', '" + results.length
-                                         + "', '" + req.file.originalname
-                                         + "')" ;
-                //console.log(sql);
-                /**/
-                db.query(sql, (error, result) => {
-                  let master_id = result.insertId;
-                  sql = `INSERT INTO import_data (master_id, Location_Path, NE_Name, NE_IP, NE_Type, Rack, Shelf, Slot, Port, ONU_ID, 
-                    Name, ONU_Configured_Type, Vendor_Name, ONU_Ranging, Service_Type, Administrative_Status,Operational_Status, 
-                    Authentication_Mode, Authentication_Value,  MAC_SN, Installation_Date, Received_Optical_Power, 
-                    Transmitted_Optical_Power, Temperature, Voltage, Bias_Current, OLT_Received_Optical_Power )
-                        VALUES ( ` + master_id + `,?, ?, ?, ?, ?, ?, ?, ?, ?, 
-                                 ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, 
-                                 ?, ?, ?, ?, ?, ?, ? )`;
-    		  console.log('-- Got results row = ' + results.length);
-          var nc_data = 0;
-		  results.forEach((ele) => {
-                    if(ele['Receive Optical Power(dBm)'] != '--') {
-                      let _value = Object.values(ele);
-                      //_value.unshift(master_id);
-                      // patch some data to remove dash -
-                      _value[0] = _value[0].replace(/-/g, '_');
-                      _value[1] = _value[1].replace(/-/g, '_');
-                      _value[2] = _value[2].replace(/-/g, '_');
-                      //console.log(_value);
-                      db.query(sql, _value , (error, _res, fields)=>{
-                        if(error) {
-                          console.log('-- DB Insert failed:' +  error);
-                        }
-                      })
-                    } else {
-                      //console.log('-- This value can not insert ' + ele['MAC/SN']);
-                      nc_data++;
-                    }
-                  })
-                  db.query("UPDATE aarx_master SET remark = '"+ nc_data + "' WHERE id = " + master_id);
-                  res.render('show_upload', { upfile: req.file, qty : results.length, nc: nc_data });
-                })
+        let sql = "INSERT INTO aarx_master (uuid,filename,qty,original_filename) VALUES ( '"
+                                  + uuid
+                                  + "', '" + req.file.filename
+                                  + "', '" + results.length
+                                  + "', '" + req.file.originalname
+                                  + "')" ;
+        //console.log(sql);
+        /**/
+        db.query(sql, (error, result) => {
+          let master_id = result.insertId;
+          sql = `INSERT INTO import_data (master_id, Location_Path, NE_Name, NE_IP, NE_Type, Rack, Shelf, Slot, Port, ONU_ID, 
+            Name, ONU_Configured_Type, Vendor_Name, ONU_Ranging, Service_Type, Administrative_Status,Operational_Status, 
+            Authentication_Mode, Authentication_Value,  MAC_SN, Installation_Date, Received_Optical_Power, 
+            Transmitted_Optical_Power, Temperature, Voltage, Bias_Current, OLT_Received_Optical_Power )
+                VALUES ( ` + master_id + `,?, ?, ?, ?, ?, ?, ?, ?, ?, 
+                          ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, 
+                          ?, ?, ?, ?, ?, ?, ? )`;
+        console.log('-- Got results row = ' + results.length);
+        let nc_data = 0;
+        let dups_list = [];
+        for(ele of results) {
+          //console.log(ele);
+          /* if(dups_list.find(name => name == ele['ONU Name'])) {
+            ///console.log('dups = ' + ele['ONU Name']);
+          } else { */
+            if(ele['Receive Optical Power(dBm)'] != '--') {
+              let _value = Object.values(ele);
+              //_value.unshift(master_id);
+              // patch some data to remove dash -
+              _value[0] = _value[0].replace(/-/g, '_');
+              _value[1] = _value[1].replace(/-/g, '_');
+              _value[2] = _value[2].replace(/-/g, '_');
+              //console.log(_value);
+              db.query(sql, _value , (error, _res)=>{
+                if(error) {
+                  console.log('-- DB Insert failed:' +  error);
+                }
+              }) 
+            } else {
+              //console.log('-- This value can not insert ' + ele['MAC/SN']);
+              nc_data++;
+            }
+            /* dups_list.push(ele['ONU Name']);
+          } */
+        }
+        db.query("UPDATE aarx_master SET remark = '"+ nc_data + "', NRSSP = '"+ dups_list.length + "' WHERE id = " + master_id);
+        res.render('show_upload', { upfile: req.file, qty : results.length, nc: nc_data, dup_count : dups_list.length });
+      })
   	});
       }
     } catch(err) {
